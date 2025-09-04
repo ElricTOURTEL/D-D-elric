17207e5d8a366ee1accc2f91d72ca934110aa56f
ElricTOURTEL <tourtel.elric@gmail.com>
2025-09-04 12:07:51 +0200

    add method d20 pour le futur

--- src/core/Dice.java
+++ src/core/Dice.java
@@ -5,4 +5,5 @@ import java.util.concurrent.ThreadLocalRandom;
 public final class Dice {
     private Dice() {}
     public static int d6() { return ThreadLocalRandom.current().nextInt(1,7); }
+    public static int d20(){ return ThreadLocalRandom.current().nextInt(1, 21); }
 }
